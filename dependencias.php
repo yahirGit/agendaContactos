82361703527e943b2c02f2dda71aec2bf8fc2b0c
yahirGit <yabsolano@hotmail.com>
2021-11-10 23:22:03 -0500

    version 0.1

---
+++ dependencias.php
@@ -0,0 +1,16 @@
+
+
+
+
+
+<link rel="stylesheet" type="text/css" href="public/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="public/fontawesome-free/css/all.css">
+<link rel="stylesheet" type="text/css" href="public/datatables/dataTables.bootstrap5.min.css">
+
+
+<script src="public/jquery-3.6.0.min.js"></script>
+<script src="public/bootstrap/popper.min.js"></script>
+<script src="public/bootstrap/bootstrap.min.js"></script>
+<script src="public/datatables/js/jquery.dataTables.min.js"></script>
+<script src="public/datatables/js/dataTables.bootstrap5.min.js"></script>
+<script src="public/sweetalert.min"></script>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" type="image/png" href="public/images/cuaderno.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Document</title>
+    <?php require_once "dependencias.php"; ?>
+
+</head>
+<body>
+<h1>Bienvenido</h1>
+<div class="container">
+  <div class="row">
+    <div class="col">
+      1 of 2
+    </div>
+    <div class="col">
+      2 of 2
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      1 of 3
+    </div>
+    <div class="col">
+      2 of 3
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
+  </div>
+</div>
+    
+</body>
+</html>
